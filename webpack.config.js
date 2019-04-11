fc2539fa38d48e058c774cc6ab60673f4ab05985
Juanan Antona <juan.antonadelval@gmail.com>
2019-04-11 11:10:00 +0200

    Added comments to webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,37 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const outputDirectory = 'dist';
 
 module.exports = {
+  // Where the application starts executing and webpack starts bundling
+  // 'babel-polyfill': is added to support extended JS features as async/await
   entry: ['babel-polyfill', './src/client/index.js'],
+  // The build folder
   output: {
     path: path.join(__dirname, outputDirectory),
     filename: 'bundle.js'
   },
   module: {
+    // Following, the rules to be executed by order
+    // Loaders: transformations that are applied on the source code of a module
     rules: [{
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000'
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader'
       }
-    ]
+    },
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    {
+      // 'url-loader': works like 'file' loader except that it embeds assets
+      // smaller than specified limit in bytes as data URLs to avoid requests.
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loader: 'url-loader',
+      options: {
+        limit: 100000
+      }
+    }]
   },
   resolve: {
     extensions: ['*', '.js', '.jsx']
@@ -39,7 +48,9 @@ module.exports = {
     }
   },
   plugins: [
+    // Remove outputDirectory before a new bundle
     new CleanWebpackPlugin([outputDirectory]),
+    // Generates an `index.html` file with the <script> injected.
     new HtmlWebpackPlugin({
       template: './public/index.html',
       favicon: './public/favicon.ico'
